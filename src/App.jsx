c36e3b0194af85f8709829905368452e8b75af3d
Riudiu <lacvert13@gmail.com>
2023-09-11 22:05:55 +0900

    settings ✨

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <section>1</section>
+      <section>2</section>
+      <section>3</section>
+      <section>4</section>
+      <Footer />
+    </div>
+  );
+}
